4987e5ae3b22b69988c83fd2115814a8b130016a
ssrinivaas97 <sunilsrinivaas.kg@outlook.com>
2025-09-24 22:20:04 -0700

    Url and contollers changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://streamify-app-video-conf.onrender.com",
     credentials: true, // allow frontend to send cookies
   })
 );
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://streamify-app-video-conf.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
